6f323292994e1c177f3f48eaf0c97fef27b1926b
Ramin Rezaei <ramin12207@gmail.com>
2020-12-18 19:03:38 +0330

    feat: tsc jest
    add jest
    config tsc to scr as root directory
    config jest to knowing src too !
    add some experimental test on index page
    new .babelrc config

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from '@jest/types'
+
+const config: Config.InitialOptions = {
+  roots: ['<rootDir>'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx'],
+  testPathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|.next)[/\\\\]'],
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'babel-jest',
+  },
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
+  },
+  moduleDirectories: ['node_modules', 'src'],
+}
+
+export default config
