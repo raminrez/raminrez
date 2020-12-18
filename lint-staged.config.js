8f45625133cab70340f0173d66e227f01d7bb1f4
Ramin Rezaei <ramin12207@gmail.com>
2020-12-19 01:15:50 +0330

    feat: stylelint pre-commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  // Run type-check on changes to TypeScript files
-  '**/*.ts?(x)': () => 'yarn type-check',
-  // Run ESLint on changes to JavaScript/TypeScript files
-  '**/*.(ts|js)?(x)': (filenames) => `yarn lint ${filenames.join(' ')}`,
   '*': 'pretty-quick  --staged',
+  '**/*.ts?(x)': () => 'yarn type-check',
+  '**/*.(ts|js)?(x)': (filenames) => {
+    const files = filenames.join(' ')
+    return [`yarn lint ${files} `, ` yarn stylelint ${files}`]
+  },
 }
