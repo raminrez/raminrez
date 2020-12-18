45af5eeadf01d88ae0db86120a30d5523d130564
Ramin Rezaei <ramin12207@gmail.com>
2020-12-18 22:06:43 +0330

    feat: chakra
    add chakra-ui
    remove css styles
    new api return🥸
    
    [chakra-ui have some issues with typescript!]

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import { AppProps } from 'next/app'
-import 'styles/globals.css'
+import { Box, ChakraProvider } from '@chakra-ui/react'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Box bg="blue.900" h="full">
+        <Component {...pageProps} />
+      </Box>
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
