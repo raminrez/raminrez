24f9b654c84a043dd1a5396a92ba6834b502dadb
Ramin Rezaei <ramin12207@gmail.com>
2020-12-19 10:54:56 +0330

    feat: mdx testUtils
    testUtils:
    fix wrong file extension
    wrapper: ChakraProvider
    MDX:
    -config next for mdx
    -type definition
    -config tsconfig.json

---
+++ src/utils/testUtils.tsx
@@ -0,0 +1,18 @@
+import { render, RenderOptions } from '@testing-library/react'
+import { ChakraProvider } from '@chakra-ui/react'
+import { FC } from 'react'
+// import { TranslationProvider } from "my-i18n-lib"
+// import defaultStrings from "i18n/en-x-default"
+
+const Providers: FC<any> = ({ children }) => {
+  return <ChakraProvider>{children}</ChakraProvider>
+}
+
+const customRender = (ui, options: RenderOptions | undefined = {}) =>
+  render(ui, { wrapper: Providers, ...options })
+
+// re-export everything
+export * from '@testing-library/react'
+
+// override render method
+export { customRender as render }
